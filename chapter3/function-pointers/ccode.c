f892dd969ecc96b2dd52a4d9dace855bfdf334f9
Philip Herron <herron.philip@googlemail.com>
2015-10-30 15:15:25 +0000

    Remove old example

--- chapter3/function-pointers/ccode.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "ccode.h"
-
-int c_main (struct function * f)
-{
-  printf ("Going to try and exec %s\n", f->name);
-  return f->ptr ();
-}
--- chapter3/function-pointers/cpycode.pxd
+++
@@ -1,5 +0,0 @@
-cdef extern from "ccode.h":
-    struct function:
-        char * name
-        int (*ptr)()
-    int c_main (function *)
